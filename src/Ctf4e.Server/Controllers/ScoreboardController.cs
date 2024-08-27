a62951c3c27aed52a75a5e141c9355552d0c760e
Jan Wichelmann <code@jwichelmann.de>
2024-08-27 23:52:45 +0200

    Refactor scoreboard view

--- src/Ctf4e.Server/Controllers/ScoreboardController.cs
+++ src/Ctf4e.Server/Controllers/ScoreboardController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Ctf4e.Server.Authorization;
 using Ctf4e.Server.Constants;
 using Ctf4e.Server.Services;
@@ -19,60 +20,77 @@ public class ScoreboardController(IUserService userService, IScoreboardService s
 {
     protected override MenuItems ActiveMenuItem => MenuItems.Scoreboard;
 
-    private async Task<IActionResult> RenderAsync(ViewType viewType, string viewPath)
+    private async Task<IActionResult> RenderScoreboardAsync(Scoreboard scoreboard)
     {
         var labService = HttpContext.RequestServices.GetRequiredService<ILabService>();
         var slotService = HttpContext.RequestServices.GetRequiredService<ISlotService>();
 
         ViewData["Labs"] = await labService.GetLabsAsync(HttpContext.RequestAborted);
         ViewData["Slots"] = await slotService.GetSlotsAsync(HttpContext.RequestAborted);
+        
+        return await RenderViewAsync("~/Views/Scoreboard/Index.cshtml", scoreboard);
+    }
 
-        ViewData["ViewType"] = viewType;
-        return await RenderViewAsync(viewPath);
+    private Task<IActionResult> RenderBlankPageAsync()
+    {
+       return RenderViewAsync("~/Views/Scoreboard/Empty.cshtml"); 
     }
 
     [HttpGet("")]
-    public async Task<IActionResult> RenderScoreboardAsync(int? labId, int? slotId, int reload = 0, bool showAllEntries = false, bool resetCache = false)
+    public async Task<IActionResult> RenderScoreboardAsync(int? labId, int? slotId, int reload = 0)
     {
+        var currentUser = await GetCurrentUserAsync();
+        
+        // Extract and pass view flags
+        Request.Cookies.TryGetValue("ScoreboardViewFlags", out var viewFlagsCookie);
+        if(!int.TryParse(viewFlagsCookie, out int viewFlagsInt))
+            viewFlagsInt = 0;
+        
+        var viewFlags = (ViewFlags)viewFlagsInt;
+        if(!currentUser.Privileges.HasPrivileges(UserPrivileges.ViewAdminScoreboard))
+            viewFlags &= ~(ViewFlags.ShowAllEntries | ViewFlags.BypassCache);
+        
+        ViewData["ViewFlags"] = viewFlags;
+        
+        bool bypassCache = (viewFlags & ViewFlags.BypassCache) != 0;
+        
+        Scoreboard scoreboard;
         using(MiniProfiler.Current.Step("Retrieve scoreboard"))
         {
-            Scoreboard scoreboard;
             if(labId == null)
             {
-                scoreboard = await scoreboardService.GetFullScoreboardAsync(slotId, HttpContext.RequestAborted, resetCache);
+                scoreboard = await scoreboardService.GetFullScoreboardAsync(slotId, HttpContext.RequestAborted, bypassCache);
             }
             else
             {
-                scoreboard = await scoreboardService.GetLabScoreboardAsync(labId.Value, slotId, HttpContext.RequestAborted, resetCache);
+                scoreboard = await scoreboardService.GetLabScoreboardAsync(labId.Value, slotId, HttpContext.RequestAborted, bypassCache);
                 if(scoreboard == null)
                 {
                     AddStatusMessage(StatusMessageType.Info, Localizer["RenderScoreboardAsync:EmptyScoreboard"]);
-                    return await RenderAsync(ViewType.Blank, "~/Views/Scoreboard.cshtml");
+                    return await RenderBlankPageAsync();
                 }
             }
 
             if(scoreboard.Entries.Count == 0)
             {
                 AddStatusMessage(StatusMessageType.Info, Localizer["RenderScoreboardAsync:EmptyScoreboard"]);
-                return await RenderAsync(ViewType.Blank, "~/Views/Scoreboard.cshtml");
+                return await RenderBlankPageAsync();
             }
-
-            ViewData["Scoreboard"] = scoreboard;
         }
 
-        var currentUser = await GetCurrentUserAsync();
-        ViewData["ShowAllEntries"] = showAllEntries && currentUser.Privileges.HasPrivileges(UserPrivileges.ViewAdminScoreboard);
-        ViewData["ResetCache"] = resetCache && currentUser.Privileges.HasPrivileges(UserPrivileges.Admin);
-
         if(reload > 0 && currentUser.Privileges.HasPrivileges(UserPrivileges.ViewAdminScoreboard))
+        {
             Response.Headers["Refresh"] = reload.ToString();
+            ViewData["AutoReload"] = reload;
+        }
 
-        return await RenderAsync(ViewType.Scoreboard, "~/Views/Scoreboard.cshtml");
+        return await RenderScoreboardAsync(scoreboard);
     }
-
-    public enum ViewType
+    
+    [Flags]
+    public enum ViewFlags
     {
-        Blank,
-        Scoreboard
+        ShowAllEntries = 1,
+        BypassCache = 2
     }
 }
--- src/Ctf4e.Server/wwwroot/js/main.js
+++ src/Ctf4e.Server/wwwroot/js/main.js
@@ -7,3 +7,60 @@ function switchLanguage(element)
     document.cookie = `${cookieName}=${encodeURIComponent(selectedLanguage)}; expires=${expires}; samesite=lax; path=/`;
     location.reload();
 }
+
+// Utility function: Reads the given cookie's value.
+function getCookie(name, defaultValue = undefined)
+{
+    // From https://stackoverflow.com/a/15724300
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+
+    let result = defaultValue;
+    if(parts.length === 2)
+        result = parts.pop().split(';').shift();
+
+    return result;
+}
+
+// Utility function: Sets the given cookie's value.
+function setCookie(name, value, maxAge = null)
+{
+    if(maxAge === null)
+        document.cookie = `${name}=${value}; path=/; samesite=lax`;
+    else
+        document.cookie = `${name}=${value}; path=/; samesite=lax; max-age=${maxAge}`;
+}
+
+// Utility function: Retrieve a view flag from a cookie.
+function getViewFlag(cookieName, flag)
+{
+    let cookie = getCookie(cookieName);
+    if(cookie)
+        return (Number(cookie) & flag) === flag;
+
+    return false;
+}
+
+// Utility function: Toggle a view flag in a cookie.
+function toggleViewFlag(cookieName, flag)
+{
+    let cookie = getCookie(cookieName);
+    if(!cookie)
+        cookie = flag;
+    else
+        cookie = Number(cookie) ^ flag;
+
+    setCookie(cookieName, cookie, 3600);
+}
+
+// Sets up a view flag switch for the given cookie and flag.
+function setupViewFlagSwitch(cookieName, sw, flag)
+{
+    sw.addEventListener("change", () => {
+        toggleViewFlag(cookieName, flag);
+
+        if(window.history.replaceState )
+            window.history.replaceState(null, null, window.location.href);
+        window.location = window.location.href;
+    });
+}
